c89e131448750fdb3b3f6c1d201e7309d7b9e600
Alexander Pérez <alexperez4016@gmail.com>
2026-03-25 12:05:03 -0300

    Clase2

---
+++ src/basic/b2_begineer/DataTypes.java
@@ -0,0 +1,22 @@
+package basic.b2_begineer;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Tipos de datos
+
+        int myInt = 37;
+        System.out.println(myInt);
+
+        double myDouble = 1.77;
+        System.out.println(myDouble);
+
+        char myChar = 'a';
+        System.out.println(myChar);
+
+        boolean myBoolean = false;
+        System.out.println(myBoolean);
+
+        String myString = "Dato String";
+        System.out.println(myString);
+    }
+}
